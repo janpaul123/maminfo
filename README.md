ee284a2b570babc4745f367973fe2393981fd49a
JP Posma <janpaul123@users.noreply.github.com>
2021-07-11 21:38:44 -0700

    Adapted for my use cases

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
-# maminfo
-Read LTO/Ultrium cartridges' medium auxiliary memory attributes from a tape drive
-
-This software is based on another software for reading/writing the custom text attribute in the
-auxiliary memory: https://github.com/scangeo/lto-cm/
-
-All Low-Level SCSI information was taken from the excellent IBM documentation that can be found at
-https://www-01.ibm.com/support/docview.wss?uid=ssg1S7003556&aid=1
-
-You will need sg3_utils installed to build this (Package sg3_utils-devel on RedHat'ish systems).
+My fork from https://github.com/arogge/maminfo, for my own use cases
--- mam-info.c
+++ mam-info.c
@@ -9,7 +9,34 @@
 #include <sys/stat.h>
 #include <scsi/sg_lib.h>
 #include <scsi/sg_io_linux.h>
-#include "mam-info.h"
+
+#define READ_ATT_REPLY_LEN 512
+#define READ_ATT_CMD_LEN 16
+#define EBUFF_SZ 256
+
+#define MAM_TYPE_BINARY          0x00
+#define MAM_TYPE_ASCII           0x01
+
+// https://www.ibm.com/support/pages/system/files/support/ssg/ssgdocs.nsf/0/4d430d4b4e1f09b18525787300607b1d/$FILE/LTO%20SCSI%20Reference%20(EXTERNAL%20-%2020171024).pdf
+#define MAM_ATT_MANUFACTURER     0x0400
+#define MAM_ATT_SERIAL           0x0401
+#define MAM_ATT_MANUFACTURE_DATE 0x0406
+#define MAM_ATT_LAST_WRITTEN     0x0804
+#define MAM_ATT_BARCODE          0x0806
+#define MAM_ATT_IDENTIFIER       0x0008
+#define MAM_ATT_LOAD_COUNT       0x0003
+#define MAM_ATT_INIT_COUNT       0x0007
+#define MAM_ATT_TOTAL_MB_WRITTEN 0x0220
+#define MAM_ATT_TOTAL_MB_READ    0x0221
+#define MAM_ATT_LAST_MB_WRITTEN  0x0222
+#define MAM_ATT_LAST_MB_READ     0x0223
+#define MAM_ATT_MAXIMUM_CAPACITY 0x0001
+#define MAM_ATT_DENSITY_CODE     0x0405
+
+
+struct globalArgs_t {
+  const char* device_name;
+} globalArgs;
 
 //---------------Usage--------------
 static void usage()
@@ -20,8 +47,6 @@ static void usage()
           "lto-cm -f device [-c] [-v]\n"
           " where:\n"
           "    -f device        is a sg device                        \n"
-          "    -c               compact mode\n"
-          "    -v               increase verbosity \n"
           "    -h/?             display usage\n"
          );
 }
@@ -49,7 +74,7 @@ int att_read(int fd, void* data, int command, int len, int data_type){
     io_hdr.timeout = 20000;     
 
     if (ioctl(fd, SG_IO, &io_hdr) < 0) {
-        if(globalArgs.verbose)perror("SG_READ_ATT_0803: Inquiry SG_IO ioctl error");
+        perror("SG_READ_ATT_0803: Inquiry SG_IO ioctl error");
         close(fd);
         return -1;
     }
@@ -60,28 +85,26 @@ int att_read(int fd, void* data, int command, int len, int data_type){
         ok = 1;
         break;
     case SG_LIB_CAT_RECOVERED:
-        if(globalArgs.verbose)printf("Recovered error on SG_READ_ATT, continuing\n");
+        printf("Recovered error on SG_READ_ATT, continuing\n");
         ok = 1;
         break;
     default: /* won't bother decoding other categories */
         printf("ERROR : Problem reading attribute %04x\n", command);
-        if(globalArgs.verbose)sg_chk_n_print3("SG_READ_ATT command error", &io_hdr, 1);
+        sg_chk_n_print3("SG_READ_ATT command error", &io_hdr, 1);
         return -1;
     }
 
     if (ok) { /* output result if it is available */
 
-        if(globalArgs.verbose)printf("SG_READ_ATT command=%0x duration=%u millisecs, resid=%d, msg_status=%d\n",
+        printf("SG_READ_ATT command=%0x duration=%u millisecs, resid=%d, msg_status=%d\n",
                 command, io_hdr.duration, io_hdr.resid, (int)io_hdr.msg_status);
 
-        if(globalArgs.verbose) {
         int i;
         printf("Raw value for attribute %04x: ", command);
         for(i=0;i<len;i++) {
           printf("%02x ", inBuff[9+i]);
         }
         printf("\n");
-        }
 
         if(data_type == MAM_TYPE_BINARY) {
            int i;
@@ -112,14 +135,10 @@ int main(int argc, char * argv[])
     char ebuff[EBUFF_SZ];
     char messageout[160] ;
     int c=0;
-    struct mam_medium_t medium;
 
-   globalArgs.verbose=0;
-   globalArgs.device_name=NULL;
-   globalArgs.compact=0;
+    globalArgs.device_name=NULL;
 
     while (1) {
-   
         c = getopt(argc, argv, "f:h?vc");
 
         if (c == -1)
@@ -137,12 +156,6 @@ int main(int argc, char * argv[])
         case '?':
             usage();
             return 0;
-        case 'v':
-            ++globalArgs.verbose;
-            break;
-        case 'c':
-            ++globalArgs.compact;
-            break;
         default:
             fprintf(stderr, "ERROR : Unrecognised option code 0x%x ??\n", c);
             usage();
@@ -156,20 +169,19 @@ int main(int argc, char * argv[])
         return 1;
     }   
 
-        if (optind < argc) {
-            for (; optind < argc; ++optind)
-                fprintf(stderr, "ERROR : Unexpected extra argument: %s\n",
-                        argv[optind]);
+    if (optind < argc) {
+        for (; optind < argc; ++optind)
+            fprintf(stderr, "ERROR : Unexpected extra argument: %s\n",
+                    argv[optind]);
+        usage();
+        return SG_LIB_SYNTAX_ERROR;
+    }
+
+    if(!(globalArgs.device_name)){
             usage();
             return SG_LIB_SYNTAX_ERROR;
-        }
-        
-        if(!(globalArgs.device_name)){
-                usage();
-                return SG_LIB_SYNTAX_ERROR;
-        }
-        
- 
+    }
+
     if ((sg_fd = open(globalArgs.device_name, O_RDWR)) < 0) {
         snprintf(ebuff, EBUFF_SZ,
                  "ERROR : opening file: %s", file_name);
@@ -184,88 +196,106 @@ int main(int argc, char * argv[])
         return -1;
     }
 
-        if(att_read(sg_fd, medium.manufacturer, MAM_ATT_MANUFACTURER, 8, MAM_TYPE_ASCII) < 0) {
-                printf("ERROR : Read failed (try verbose opt)\n");
-                close(sg_fd);
-                return -1;
-        }
-        if(att_read(sg_fd, medium.serial, MAM_ATT_SERIAL, 32, MAM_TYPE_ASCII) < 0) {
-                printf("ERROR : Read failed (try verbose opt)\n");
-                close(sg_fd);
-                return -1;
-        }
+    char manufacturer[9];
+    if(att_read(sg_fd, manufacturer, MAM_ATT_MANUFACTURER, 8, MAM_TYPE_ASCII) < 0) {
+            printf("ERROR : Read manufacturer failed\n");
+    }
+    printf("Manufacturer: %.8s\n", manufacturer);
 
-        if(att_read(sg_fd, medium.manufacture_date, MAM_ATT_MANUFACTURE_DATE, 8, MAM_TYPE_ASCII) < 0) {
-                printf("ERROR : Read failed (try verbose opt)\n");
-                close(sg_fd);
-                return -1;
-        }
+    char serial[33];
+    if(att_read(sg_fd, serial, MAM_ATT_SERIAL, 32, MAM_TYPE_ASCII) < 0) {
+            printf("ERROR : Read serial failed\n");
+    }
+    printf("Serial: %.32s\n", serial);
+
+    char manufacture_date[9];
+    if(att_read(sg_fd, manufacture_date, MAM_ATT_MANUFACTURE_DATE, 8, MAM_TYPE_ASCII) < 0) {
+            printf("ERROR : Read manufacture_date failed\n");
+    }
+    printf("Manuf. Date:  %.8s\n", manufacture_date);
+
 /*
-        if(att_read(sg_fd, medium.last_written, MAM_ATT_LAST_WRITTEN, 12, MAM_TYPE_ASCII) < 0) {
-                medium.last_written[0] = '\0';
-        }
+    char last_written[13];
+    if(att_read(sg_fd, last_written, MAM_ATT_LAST_WRITTEN, 12, MAM_TYPE_ASCII) < 0) {
+            printf("ERROR : Read last_written failed\n");
+            last_written[0] = '\0';
+    }
+    printf("Last written: %.12s", last_written);
 */
-        if(att_read(sg_fd, medium.barcode, MAM_ATT_BARCODE, 12, MAM_TYPE_ASCII) < 0) {
-                printf("ERROR : Read failed (try verbose opt)\n");
-                close(sg_fd);
-                return -1;
-        }
+    char barcode[33];
+    if(att_read(sg_fd, barcode, MAM_ATT_BARCODE, 12, MAM_TYPE_ASCII) < 0) {
+            printf("ERROR : Read barcode failed\n");
+            barcode[0] = '\0';
+    }
+    printf("Barcode: %.12s\n", barcode);
+
 /*
-        if(att_read(sg_fd, medium.identifier, MAM_ATT_IDENTIFIER, 12, MAM_TYPE_ASCII) < 0) {
-                medium.identifier[0] = '\0';
-        }
+    char identifier[33];
+    if(att_read(sg_fd, identifier, MAM_ATT_IDENTIFIER, 32, MAM_TYPE_ASCII) < 0) {
+            printf("ERROR : Read identifier failed\n");
+            identifier[0] = '\0';
+    }
+    printf("Identifier: %.32s\n", identifier);
 */
-        if(att_read(sg_fd, &medium.load_count, MAM_ATT_LOAD_COUNT, 8, MAM_TYPE_BINARY) < 0) {
-                printf("ERROR : Read failed (try verbose opt)\n");
-                close(sg_fd);
-                return -1;
-        }
-        if(att_read(sg_fd, &medium.init_count, MAM_ATT_INIT_COUNT, 2, MAM_TYPE_BINARY) < 0) {
-                printf("ERROR : Read failed (try verbose opt)\n");
-                close(sg_fd);
-                return -1;
-        }
-        if(att_read(sg_fd, &medium.total_mb_written, MAM_ATT_TOTAL_MB_WRITTEN, 8, MAM_TYPE_BINARY) < 0) {
-                printf("ERROR : Read failed (try verbose opt)\n");
-                close(sg_fd);
-                return -1;
-        }
-        if(att_read(sg_fd, &medium.total_mb_read, MAM_ATT_TOTAL_MB_READ, 8, MAM_TYPE_BINARY) < 0) {
-                printf("ERROR : Read failed (try verbose opt)\n");
-                close(sg_fd);
-                return -1;
-        }
+
+/*
+    uint64_t load_count;
+    if(att_read(sg_fd, &load_count, MAM_ATT_LOAD_COUNT, 8, MAM_TYPE_BINARY) < 0) {
+            printf("ERROR : Read failed\n");
+    }
+*/
+    uint16_t init_count;
+    if(att_read(sg_fd, &init_count, MAM_ATT_INIT_COUNT, 2, MAM_TYPE_BINARY) < 0) {
+            printf("ERROR : Read init_count failed\n");
+    }
+    printf("Init count: %d\n", init_count);
+
+    uint64_t total_mb_written;
+    if(att_read(sg_fd, &total_mb_written, MAM_ATT_TOTAL_MB_WRITTEN, 8, MAM_TYPE_BINARY) < 0) {
+            printf("ERROR : Read total_mb_written failed\n");
+    }
+    printf("Total MB written: %ld\n", total_mb_written);
+
+    uint64_t total_mb_read;
+    if(att_read(sg_fd, &total_mb_read, MAM_ATT_TOTAL_MB_READ, 8, MAM_TYPE_BINARY) < 0) {
+            printf("ERROR : Read total_mb_read failed\n");
+    }
+    printf("Total MB read: %ld\n", total_mb_read);
+
+    uint64_t last_mb_written;
+    if(att_read(sg_fd, &last_mb_written, MAM_ATT_LAST_MB_WRITTEN, 8, MAM_TYPE_BINARY) < 0) {
+            printf("ERROR : Read last_mb_written failed\n");
+    }
+    printf("Last MB written: %ld\n", last_mb_written);
+
+    uint64_t last_mb_read;
+    if(att_read(sg_fd, &last_mb_read, MAM_ATT_LAST_MB_READ, 8, MAM_TYPE_BINARY) < 0) {
+            printf("ERROR : Read last_mb_read failed\n");
+    }
+    printf("Last MB read: %ld\n", last_mb_read);
+
+    /*
+    uint64_t maximum_capcity;
+    if(att_read(sg_fd, &maximum_capcity, MAM_ATT_MAXIMUM_CAPACITY, 8, MAM_TYPE_BINARY) < 0) {
+            printf("ERROR : Read maximum_capcity failed\n");
+    }
+    printf("Maximum capacity in MB: %ld\n", maximum_capcity);
+    */
+
+    // ULTRIUM 3: 44h
+    // ULTRIUM 4: 46h
+    // ULTRIUM 5: 58h
+    // ULTRIUM 6: 5Ah
+    // ULTRIUM 7: 5Ch
+    // ULTRIUM M8: 5Dh
+    // ULTRIUM 8: 5Eh
+    char density_code;
+    if(att_read(sg_fd, &density_code, MAM_ATT_DENSITY_CODE, 1, MAM_TYPE_BINARY) < 0) {
+            printf("ERROR : Read density_code failed\n");
+    }
+    printf("Density code: %02X\n", density_code);
+
     close(sg_fd);
 
-        char fmtString[] = 
-                "Barcode:      %.8s\n"
-                "Manufacturer: %.8s\n"
-                "Serial:       %.32s\n"
-                "Manuf. Date:  %.8s\n"
-                "Load count:   %d\n"
-                "Init count:   %d\n"
-                "MB written:   %d\n"
-                "MB read:      %d\n";
-
-        char compactFmtString[] =
-                "%.8s "
-                "%.8s "
-                "%.10s "
-                "%.8s\t"
-                "%d\t"
-                "%d\t"
-                "%d\t"
-                "%d\n";
-
-        printf(globalArgs.compact ? compactFmtString : fmtString, \
-                medium.barcode,
-                medium.manufacturer,
-                medium.serial,
-                medium.manufacture_date,
-                medium.load_count,
-                medium.init_count,
-                medium.total_mb_written,
-                medium.total_mb_read
-                );
     return 0;
 }
